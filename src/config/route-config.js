9269a31f42e7290c47cde031817bb834f121df5d
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2019-01-02 16:57:33 -0600

    scaffolding changes

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,9 @@
-module.exports = {
+const logger = require('morgan');
+
+ module.exports =  {
     init(app){
-        const staticRoutes = require("../routes/static");
-        app.use(staticRoutes);
+      const staticRoutes = require("../routes/static");
+      app.use(staticRoutes);	       
+      app.use(logger('dev'));
     }
 }
