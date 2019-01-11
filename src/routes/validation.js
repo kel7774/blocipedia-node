3d65ab64733a9f279936bfafa6d91622e60c3116
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2019-01-11 15:25:10 -0600

    Minor changes

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,18 +1,30 @@
 module.exports = {
-    validateUsers(req, res, next){
-        if(req.method === "POST"){
-            req.checkBody("name", "Must be at least 2 characters in length").isLength({min: 2})
-            req.checkBody("email", "Must be valid").isEmail();
-            req.checkBody("password", "Must be at least 6 characters in length").isLength({min: 6})
-            req.checkBody("password_conf", "Must match password provided").optional().matches(req.body.password)
+    validateUsers(req, res, next) {
+        if(req.method === "POST") {
+            req.checkBody('name', 'must be at least 2 characters in length').isLength({min: 2})
+            req.checkBody("email", "must be valid").isEmail();
+            req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
+            req.checkBody("password_conf", "must match password provided").optional().matches(req.body.password);
         }
         const errors = req.validationErrors();
-        console.log(errors);
-        if(errors){
-            req.flash("error", errors);
-            return res.redirect(req.headers.referer);
+        if (errors) {
+          req.flash("error", errors);
+          return res.redirect(req.headers.referer);
         } else {
-            return next();
+          return next();
+        }
+    },
+    validateUser(req, res, next) {
+        if(req.method === "POST") {
+            req.checkBody("email", "must be valid").isEmail();
+            req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
+        }
+        const errors = req.validationErrors();
+        if (errors) {
+          req.flash("error", errors);
+          return res.redirect(req.headers.referer);
+        } else {
+          return next();
         }
     }
 }
