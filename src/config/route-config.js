5268de5efd34ac51c055332621368ca2c111fc68
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2019-01-12 15:56:38 -0600

    Start CRUD on Wikis

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,8 +3,10 @@ module.exports =  {
       const logger = require("morgan");
       const staticRoutes = require("../routes/static");
       const userRoutes = require("../routes/users");
+      const wikiRoutes = require("../routes/wikis");
       app.use(staticRoutes);
-      app.use(userRoutes);       
+      app.use(userRoutes);
+      app.use(wikiRoutes);
       app.use(logger('dev'));
     }
 }
