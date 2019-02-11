c52e3453473ddda4f18bb06e75f968a7491dd462
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2019-02-11 17:09:20 -0600

    do a pull, add collabs

--- src/controllers/collaboratorController.js
+++ src/controllers/collaboratorController.js
@@ -3,45 +3,7 @@ const Authorized = require("../policies/application");
 const wikiQueries = require("../db/queries.wikis");
 
 module.exports = {
-    show(req, res, next){
 //get a list of all collaborator objects assc with req.params.userId
  //query Collaborator.where({userId: req.params.userId})
  //render the result. 
-        wikiQueries.getWiki(req.params.wikiId, (err, result) => {
-            wiki = result["wiki"];
-            collaborators = result["collaborators"];
-            if(err || wiki == null){
-                res.redirect(404, "/");
-            } else {
-                const authorized = new Authorized(req.user, wiki, collaborators).edit();
-                if(authorized){
-                    res.render("collaborators/show", {wiki, collaborators});
-                } else {
-                    req.flash("notice", "You are not authorized to do that.");
-                    res.redirect(`/wikis/${req.params.wikiId}`);
-                }
-            }
-        })
-    },
-    create(req, res, next){
-        collaboratorQueries.addCollaborator(req, (err, collaborator) => {
-            if(err){
-                req.flash("notice", "User already exists")
-            }
-            res.redirect(`/wikis/${req.params.wikiId}/collaborators`);
-        })
-    },
-    destroy(req, res, next){
-        if(req.user){
-            collaboratorQueries.removeCollaborator(req, (err, collaborator) => {
-                if(err){
-                    req.flash("error", err);
-                }
-                res.redirect(req.headers.referer);
-            })
-        } else {
-            req.flash("notice", "You must be signed int to perform this action.");
-            res.redirect(req.headers.referer);
-        }
-    }
 }
--- src/routes/collaborators.js
+++ src/routes/collaborators.js
@@ -2,9 +2,4 @@ const express = require("express");
 const router = express.Router();
 const collaboratorController = require("../../src/controllers/collaboratorController");
 
-
-router.get("/users/:userId/collaborations", collaboratorController.show);
-router.post("/wikis/:wikiId/collaborators/create", collaboratorController.create);
-router.post("/wikis/:wikiId/collaborators/destroy", collaboratorController.destroy);
-
 module.exports = router;
