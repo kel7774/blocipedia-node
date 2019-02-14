4c391bd97624e963eb21e0479471b2e95efe0ae9
Kelli Landry <kellilandry@Kellis-Mac-mini.local>
2019-02-14 15:28:01 -0600

    Refactor to include authorizations in updateWiki in queries

--- src/db/queries.wikis.js
+++ src/db/queries.wikis.js
@@ -72,15 +72,28 @@ module.exports = {
               if(!wiki){
                   return callback("Wiki not found.");
               }
-              wiki.update(updatedWiki, {
-                  fields: Object.keys(updatedWiki)
-              })
-              .then(() => {
-                  callback(null, wiki);
-              })
-              .catch((err) => {
-                  callback(err);
-              });
+
+              let authorized;
+              if(wiki.private == false){
+                  authorized = new Public(req.user, wiki).update();
+              } else {
+                  authorized = new Private(req.user, wiki).update();
+              }
+
+              if(authorized){
+                wiki.update(updatedWiki, {
+                    fields: Object.keys(updatedWiki)
+                })
+                .then(() => {
+                    callback(null, wiki);
+                })
+                .catch((err) => {
+                    callback(err);
+                });
+              } else {
+                  req.flash("notice", "You are not authorized to do that.");
+                  callback("This action is forbidden.");
+              }
           });
       }
 }
